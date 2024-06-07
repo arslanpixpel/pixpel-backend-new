da7a5f373a7e13572c13bf2aa7cdff4d291370e8
hassanpixpel <hassan@pixpel.io>
2024-06-07 14:42:23 +0500

    Fix: Cron issue

--- src/routes/nftRoutes.ts
+++ src/routes/nftRoutes.ts
@@ -29,6 +29,7 @@ cron.schedule("0 * * * *", async () => {
             nft.secondary_owner.length > 0 &&
             nft.secondary_owner.some((owner: any) => owner.insurance === true)
         );
+      console.log(polygonInsuranceNfts, "polygonInsuranceNfts");
 
       for (const nft of polygonInsuranceNfts) {
         const updatedSecondaryOwners = nft.secondary_owner.filter(
@@ -38,18 +39,17 @@ cron.schedule("0 * * * *", async () => {
               new Date(owner.insurance_expirydate) <= currentDate
             )
         );
-
+        console.log(updatedSecondaryOwners, "updatedSecondaryOwners");
         if (updatedSecondaryOwners.length !== nft.secondary_owner.length) {
           // Update the secondary_owner array in the NFT table
           await query("UPDATE nfts SET secondary_owner = $1 WHERE id = $2", [
             updatedSecondaryOwners,
             nft.id,
           ]);
-
           // Update seller_wallet and listing field in the nftmarket table
           await query(
-            "UPDATE nftmarket SET seller_wallet = $1, listing = $2 WHERE id = $3",
-            [nft.primary_wallet, true, nft.id]
+            "UPDATE nftmarket SET seller = $1, listing = $2 WHERE id = $3",
+            [nft.primary_owner, true, nft.id]
           );
         }
       }
